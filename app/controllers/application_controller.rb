bb278b97d2903e5d4b0c693a2bf0e32430e05675
Gerrit Wanderer <gerrit@the-wanderer.de>
2014-04-01 21:19:57 +0200

    Add language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  def set_locale
+    if ['de', 'en'].include?(params[:locale])
+      I18n.locale = params[:locale]
+    else
+      client_locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      I18n.locale = ['de', 'en'].include?(client_locale) ? client_locale : I18n.default_locale
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_to_switch_locale
+    if I18n.locale.to_s == 'en'
+      return link_to t('header.german'), root_url(locale: :de)
+    else
+      return link_to t('header.english'), root_url(locale: :en)
+    end
+  end
 end
